2de7e0fb0f79511c19055c83b2f5f8e5d397a1dd
Shah Rukh Abdullah <samansaeed2306@gmail.com>
2024-04-16 02:27:13 +0500

    Complete Snake Game (Basic Features)

---
+++ Assets/Snake.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private Vector2 _direction = Vector2.right;
+    private List<Transform> _segments;
+    public Transform segmentPrefab;
+    private void Start()
+    {
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _direction = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            _direction = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            _direction = Vector2.left; // Change Vector2.right to Vector2.left for left direction
+        }
+        else if (Input.GetKeyDown(KeyCode.D)) // Change from KeyCode.Q to KeyCode.D for right direction
+        {
+            _direction = Vector2.right; // Change Vector2.left to Vector2.right for right direction
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.position.x) + _direction.x, // Fix the syntax here
+            Mathf.Round(this.transform.position.y) + _direction.y, // Fix the syntax here
+            0.0f
+        );
+    }
+    // transform.Translate(_direction * Time.fixedDeltaTime);
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+        _segments.Add(segment);
+    }
+    private void ResetPosition()
+    {
+        for (int i = 1; i < _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+
+        }
+        _segments.Clear();
+        _segments.Add(this.transform);
+        this.transform.position = Vector3.zero;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food")
+        {
+            Grow();
+        }
+        else if (other.tag == "Obstacle")
+        {
+            ResetPosition();
+        }
+    }
+}
